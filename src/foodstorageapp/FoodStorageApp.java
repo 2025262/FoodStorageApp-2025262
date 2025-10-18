870db9337fd4e8438deb4884f0d5f0c59cea5cdd
gabirichardz <gabi.richardz@gmail.com>
2025-10-18 14:09:05 +0100

    Second Commit
    Add food class with attrubutes

--- src/foodstorageapp/FoodStorageApp.java
+++ src/foodstorageapp/FoodStorageApp.java
@@ -6,8 +6,36 @@ package foodstorageapp;
 
 /**
  *
- * @author gabi.richardz
+ * @author Gabriela Richardz Nunes - 2025262
  */
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+//class to declare food information
+class Food {
+    private String name;
+    private int weight; // grams
+    private LocalDate bestBefore;
+    private LocalDateTime placedTime;
+
+    public Food(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+        this.bestBefore = LocalDate.now().plusWeeks(2);
+        this.placedTime = LocalDateTime.now();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Food{name='" + name + "', weight=" + weight + "g, bestBefore=" + bestBefore + ", placedTime=" + placedTime + "}";
+    }
+}
+
 public class FoodStorageApp {
 
     /**
